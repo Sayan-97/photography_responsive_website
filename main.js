7d5c91d433f06b3a6014faa9b20f0a11f2c183b2
Sayan Dey <111982005+Sayan-97@users.noreply.github.com>
2022-10-19 04:50:52 +0530

    Added About and Gallery Section

--- main.js
+++ main.js
@@ -1,5 +1,39 @@
 // change nav style on scroll
-window.addEventListener('scroll', () => {
-    document.querySelector('nav').classList.toggle('window-scrolled', window.scrollY > 0);
-})
 
+window.addEventListener("scroll", () => {
+  document
+    .querySelector("nav")
+    .classList.toggle("window-scrolled", window.scrollY > 0);
+});
+
+// CONTACT BUTTONS
+
+const textButtons = document.querySelectorAll(".contact__btn");
+
+textButtons.forEach((textButton) => {
+  let text = textButton.querySelector("p");
+  text.innerHTML = text.innerHTML
+    .split("")
+    .map(
+      (character, index) =>
+        `<span style="transform: rotate(${index * 12}deg)">${character}</span>`
+    )
+    .join('');
+});
+
+// SWIPER JS(Gallery Section)
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  slidesPerGroup: 3,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
